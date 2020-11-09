b58beda7c0ffd08e11ce40123cae406b16bce36c
Cody Bloemhard <cblmhrd@live.nl>
2020-11-09 20:42:51 +0100

    stamps and more info

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use hound;
+use std::time::Duration;
+use std::time::Instant;
 
 fn main() {
     println!("Henlo!");
+    let max = 100;
+    let verbose = true;
+    let start = Instant::now();
+    let mut till = 0;
     let mut reader = hound::WavReader::open("/home/cody/temp/180101_0006.wav").expect("bruh");
     let mut copy = Vec::new();
     for s in reader.samples::<i16>(){
@@ -9,6 +15,7 @@ fn main() {
         let s = s.unwrap();
         copy.push(s);
     }
+    stamp(&start, &mut till, "Copying");
     let mut hist = vec![0usize; 2048];
     let mut total = 0;
     for s in &copy{
@@ -16,18 +23,11 @@ fn main() {
         hist[i as usize] += 1usize;
         total += 1;
     }
+    stamp(&start, &mut till, "Histogram");
     println!("Total samples: {}", total);
-    fn depeaked_size(hist: &Vec<usize>, max: usize) -> usize{
-        let mut i = hist.len() - 2;
-        while i > 0{
-            let c = hist[i as usize];
-            if c > max { break; }
-            i -= 1;
-        }
-        i
-    }
-    let cs = depeaked_size(&hist, 100);
-    println!("Size(100): {}", cs);
+    let cs = depeaked_size(&hist, max);
+    stamp(&start, &mut till, "Depeak scan");
+    println!("Upwards from cell {} out of {} will be clipped with max cell length > {}", cs, hist.len() - 1, max);
     let thresh = (cs << 4) as i16;
     let spec = hound::WavSpec {
         channels: 2,
@@ -36,7 +36,28 @@ fn main() {
         sample_format: hound::SampleFormat::Int,
     };
     let mut writer = hound::WavWriter::create("outp.wav", spec).unwrap();
-    for s in copy.into_iter().map(|s| s.min(thresh).max(-thresh)) {
-        writer.write_sample(s as i16).unwrap();
+    let mut diff_count = 0;
+    for (os, ns) in copy.into_iter().map(|s| (s, s.min(thresh).max(-thresh))) {
+        if ns != os { diff_count += 1; }
+        writer.write_sample(ns as i16).unwrap();
     }
+    println!("Samples clipped: {} out of {} which is 1/{} or {}%", diff_count, total, total / diff_count, diff_count as f64 / total as f64 * 100.0);
+    stamp(&start, &mut till, "Write");
+    println!("Total took {} ms", start.elapsed().as_millis());
+}
+
+fn depeaked_size(hist: &Vec<usize>, max: usize) -> usize{
+    let mut i = hist.len() - 2;
+    while i > 0{
+        let c = hist[i as usize];
+        if c > max { break; }
+        i -= 1;
+    }
+    i
+}
+
+fn stamp(start: &Instant, till: &mut u128, action: &str){
+    let elapsed = start.elapsed().as_millis();
+    println!("{} took {} ms", action, elapsed - *till);
+    *till = elapsed;
 }
